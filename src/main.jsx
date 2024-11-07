e49bb453de27fa19cc76efeec2374d07a618790c
Tochka <tochka114@gmail.com>
2024-11-07 21:27:16 +0200

    1

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
